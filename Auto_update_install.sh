430e660cd0b56877b64bfdf7c6685d52a097dd96
lucifer <>
2018-02-07 08:26:56 -0800

    silence mode

--- Auto_update_install.sh
+++ Auto_update_install.sh
@@ -1,23 +1,23 @@
 # copyright by Lucifer
 # 2017/12/3
 #!/bin/bash
-apt-get remove vim*
+apt-get remove vim* -y
 
-apt-get update
-apt-get install git
+apt-get update -y
+apt-get install git -y
 echo "install oh my zsh"
 apt-get install zsh
 #sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 echo "install tree"
-apt-get install tree
+apt-get install tree -y
 
 echo "install lrzsz"
-apt-get install lrzsz
+apt-get install lrzsz -y
 
-apt-get install unzip
-apt-get install sudo
+apt-get install unzip -y
+apt-get install sudo -y
 
 echo '/bin/zsh' >> /etc/shells
 
