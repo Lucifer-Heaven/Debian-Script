7596e90f6e16162d4238b7c3795eada0707793e6
leonidas <10078186+luc1fev@users.noreply.github.com>
2018-02-08 01:38:04 -0800

    change installation order

--- Auto_update_install.sh
+++ Auto_update_install.sh
@@ -1,12 +1,20 @@
 # copyright by Lucifer
 # 2017/12/3
 #!/bin/bash
-apt-get remove vim* -y
 
 apt-get update -y
-apt-get install git -y
+apt-get autoremove -y
+apt-get remove vim* -y
+# sometimes here's pre-installed tidy-vi,but the hot key is diff with normal vim
+
+echo "install basic tool"
+apt-get install vim sudo unzip -y
+
+echo "install version control"
+apt-get install git mercurial  -y
+
 echo "install oh my zsh"
-apt-get install zsh
+apt-get install zsh -y
 #sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
@@ -15,10 +23,6 @@ apt-get install tree -y
 
 echo "install lrzsz"
 apt-get install lrzsz -y
-
-apt-get install unzip -y
-apt-get install sudo -y
-apt-get install vim -y
 
 echo '/bin/zsh' >> /etc/shells
 
