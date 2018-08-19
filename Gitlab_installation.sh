d4dd4ef94c7df94dbef33263cc34e1c22b4a7ecd
leonidas <10078186+luc1fev@users.noreply.github.com>
2018-08-18 22:20:27 -0700

    update installation of gitlab with no default addr

---
+++ Gitlab_installation.sh
@@ -0,0 +1,10 @@
+# from https://about.gitlab.com/installation/#debian 
+# 2018/8/18
+sudo apt-get update
+sudo apt-get install -y curl openssh-server ca-certificates
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+
+sudo EXTERNAL_URL="http://gitlab.example.com" apt-get install gitlab-ee
+# enable https
+# https://docs.gitlab.com/omnibus/settings/nginx.html#enable-https
