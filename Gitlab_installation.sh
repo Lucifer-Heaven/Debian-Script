27a417a5cde94a5383c18eb417b6131f03c024fa
leonidas <10078186+luc1fev@users.noreply.github.com>
2018-08-18 22:22:33 -0700

    add local server/gitlab as git server addr

--- Gitlab_installation.sh
+++ Gitlab_installation.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y curl openssh-server ca-certificates
 
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 
-sudo EXTERNAL_URL="http://gitlab.example.com" apt-get install gitlab-ee
+sudo EXTERNAL_URL="http://192.168.0.4/gitlab" apt-get install gitlab-ee
 # enable https
 # https://docs.gitlab.com/omnibus/settings/nginx.html#enable-https
