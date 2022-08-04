0fb48bb7aa9ffcc0d16d69a9fd6d996a20127b5f
Lucifer <10078186+luc1fev@users.noreply.github.com>
2022-08-04 15:29:45 -0700

    docker updated

--- Docker_installation.sh
+++ Docker_installation.sh
@@ -1,15 +1,12 @@
 # from https://docs.docker.com/install/linux/docker-ce/debian/
-sudo apt-get remove docker docker-engine docker.io -y
-echo "remove done!"
+echo "remove remaining"
+sudo apt-get remove docker docker-engine docker.io -yqq
 
-sudo apt update
-
-sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -yqq
 echo "install essential"
-
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -yqq
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -yqq
 
@@ -17,7 +14,6 @@ echo "enable daemon"
 sudo systemctl enable --now docker
 
 echo "install docker-compose"
-
 sudo apt install curl wget -yqq
 curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
 chmod +x docker-compose-linux-x86_64
